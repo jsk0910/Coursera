2bc69c0b0cba8641ba78de37f308143a843ee635
jsk0910 <sangkyun.jeon@gmail.com>
2023-03-22 10:35:32 +0000

    update BruteCollinear

--- Algorithms-Princeton Univ/HW_04_Collinear Points/BruteCollinearPoints.java
+++ Algorithms-Princeton Univ/HW_04_Collinear Points/BruteCollinearPoints.java
@@ -8,5 +8,20 @@ public class BruteCollinearPoints {
 
     public BruteCollinearPoints(Point[] points) {
         if (points == null) throw new IllegalArgumentException();
+        
+        this.points = new Point[points.length];
+
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) throw new IllegalArgumentException();
+
+            for (int j = 0; j < i; j++)
+                if (this.points[j].compareTo(points[i]) == 0) throw new IllegalArgumentException();
+            this.points[i] = points[i];
+        }
+
+        int numOfPoints = points.length;
+        
+        List<LineSegment> lineSegments = new LinkedList<>();
+        Arrays.sort(points);
     }
 }
