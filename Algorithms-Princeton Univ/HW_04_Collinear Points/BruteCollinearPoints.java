68b83618a9bf03bc2a8a197bd2de8b150017454e
jsk0910 <sangkyun.jeon@gmail.com>
2023-03-22 10:14:03 +0000

    add linesegment and brutecollinear

---
+++ Algorithms-Princeton Univ/HW_04_Collinear Points/BruteCollinearPoints.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class BruteCollinearPoints {
+
+    private Point[] points;
+    private int numOfSeg;
+    private LineSegment[] line;
+
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) throw new IllegalArgumentException();
+    }
+}
